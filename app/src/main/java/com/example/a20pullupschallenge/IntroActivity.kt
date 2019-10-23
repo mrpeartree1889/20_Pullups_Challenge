2777dac047935c171258aa47c9ec04148fd2a02c
paulop <jan.voss@outlook.pt>
2019-10-23 11:17:30 -0500

    finished intro slider, added new icons, new shapes

--- app/src/main/java/com/example/a20pullupschallenge/IntroActivity.kt
+++ app/src/main/java/com/example/a20pullupschallenge/IntroActivity.kt
@@ -1,8 +1,9 @@
 package com.example.a20pullupschallenge
 
 import android.os.Bundle
+import android.widget.NumberPicker
 import com.github.paolorotolo.appintro.AppIntro
-
+import kotlinx.android.synthetic.main.slide_two.*
 
 class IntroActivity : AppIntro() {
 
