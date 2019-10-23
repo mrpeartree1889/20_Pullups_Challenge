4149344962d4287f4d2662f16084e31856a5048c
paulop <jan.voss@outlook.pt>
2019-10-22 20:47:46 -0500

    testing out sliders

---
+++ app/src/main/java/com/example/a20pullupschallenge/IntroActivity.kt
@@ -0,0 +1,34 @@
+package com.example.a20pullupschallenge
+
+import android.os.Bundle
+import com.github.paolorotolo.appintro.AppIntro
+
+
+class IntroActivity : AppIntro() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        addSlide(SampleSlide.newInstance(R.layout.slide_one))
+        addSlide(SampleSlide.newInstance(R.layout.slide_two))
+
+        setFadeAnimation() // animation between slides
+    }
+
+    override fun onSkipPressed() {
+        // Do something when users tap on Skip button.
+    }
+
+    override fun onNextPressed() {
+        // Do something when users tap on Next button.
+    }
+
+    override fun onDonePressed() {
+        // Do something when users tap on Done button.
+        finish()
+    }
+
+    override fun onSlideChanged() {
+        // Do something when slide is changed
+    }
+}
--- app/src/main/java/com/example/a20pullupschallenge/MainActivity.kt
+++ app/src/main/java/com/example/a20pullupschallenge/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.a20pullupschallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.R.id.edit
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.Context.MODE_PRIVATE
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+const val PREFS_FILENAME = "com.example.a20pullupschallenge.prefs"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sp = getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE)
+        if (!sp.getBoolean("first", false)) {
+            val editor = sp.edit()
+            editor.putBoolean("first", true)
+            editor.apply()
+            val intent = Intent(this, IntroActivity::class.java) // Call the AppIntro java class
+            startActivity(intent)
+        }
     }
 }
